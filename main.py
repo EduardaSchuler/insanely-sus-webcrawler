e72ad2f347930bd178eb40903e36f756ef97fa34
nickpsalles <nickpsalles@gmail.com>
2026-04-14 17:25:53 -0300

    add button_info - procura pela ul

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ sleep(1)
 #Guarda conteudo html da pagina alvo
 html_target = navegador.page_source
 
-#Faz o driver esperar por 1 segundo
-wait = WebDriverWait(navegador, 1)
+#Faz o driver esperar por x segundos(navegador, x)
+wait = WebDriverWait(navegador, 10)
 
 
-######XPATHS PARA PEGAR INFORMAÇÕES DE CADA FILME######
+######XPATHS PARA PEGAR INFORMAÇÕES DA PAGINA######
 
 #XPATH da div em que os box de cada filme estao agrupados:
 #/html/body/div[2]/main/div/div[3]/section/div/div[2]/div/ul
@@ -39,6 +39,14 @@ wait = WebDriverWait(navegador, 1)
 
 #XPATH da primeira div do primeiro box:
 #/html/body/div[2]/main/div/div[3]/section/div/div[2]/div/ul/li[1]
+#--> <li class="ipc-metadata-list-summary-item">
+
+
+#XPATH do titulo do primeiro filme:
+#absoluto:/html/body/div[2]/main/div/div[3]/section/div/div[2]/div/ul/li[1]/div/div/div/div/div[2]/ul/div/a 
+#<a href="/pt/title/tt0111161/?ref_=chttp_t_1" class="ipc-title-link-wrapper" tabindex="0">
+#--> <h3 class="ipc-title__text">Um Sonho de Liberdade</h3>
+#</a>
 
 
 #XPATH do numero de posicao do primeiro filme:
@@ -46,21 +54,23 @@ wait = WebDriverWait(navegador, 1)
 #conteudo: <div class="ipc-signpost__text" role="presentation">#1</div>
 
 
-#XPATH do titulo do primeiro filme:
-#absoluto:/html/body/div[2]/main/div/div[3]/section/div/div[2]/div/ul/li[1]/div/div/div/div/div[2]/ul/div/a
-#conteudo: <a href="/pt/title/tt0111161/?ref_=chttp_t_1" class="ipc-title-link-wrapper" tabindex="0"><h3 class="ipc-title__text">Um Sonho de Liberdade</h3></a>
-
-
 #XPATH do botao de mais informações sobre o filme:
 #/html/body/div[2]/main/div/div[3]/section/div/div[2]/div/ul/li[1]/div/div/div/div/div[3]/button
 #conteudo: <button data-testid="title-summary-prompt-button-info-icon" class="ipc-icon-button li-info-icon ipc-icon-button--base ipc-icon-button--onAccent2" title="Veja mais informações sobre Um Sonho de Liberdade" tabindex="0" aria-label="Veja mais informações sobre Um Sonho de Liberdade" aria-disabled="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="ipc-icon ipc-icon--info" viewBox="0 0 24 24" fill="currentColor" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg></button>
 
 
-#clicando no titulo do filme para acessar sua pagina
-sleep(1)
-#button_info = navegador.find_element(By.XPATH, "//*[@id="__next"]/main/div/div[3]/section/div/div[2]/div/ul/li[1]/div/div/div/div/div[2]/ul/div/a")
+#clicando no titulo do primeiro filme para acessar sua pagina
+#sem wait/EC
+#button_info = navegador.find_element(By.XPATH, '//*[@id="__next"]/main/div/div[3]/section/div/div[2]/div/ul/li[1]/div/div/div/div/div[2]/ul/div/a')
+#com wait/EC
 button_info = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__next"]/main/div/div[3]/section/div/div[2]/div/ul/li[1]/div/div/div/div/div[2]/ul/div/a')))
 button_info.click()
+
+#pegando lista de filmes
+listaDeFilmes_info = wait.until(EC.presence_of_element_located((By.XPATH, '//ul, //*[@id="__next"]#')))
+
+uls = wait.until(EC.presence_of_elements_located((By.XPATH, "//ul")))
+print(len(uls))
 
 
 '''
