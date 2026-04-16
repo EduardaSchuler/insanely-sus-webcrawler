27e335e9507313343b4b710ea4d96f8aa7611341
nickpsalles <nickpsalles@gmail.com>
2026-04-16 03:03:03 -0300

    pequenos ajustes/limpeza no código e adicionei comentarios

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 import json
 
@@ -30,7 +31,6 @@ wait = WebDriverWait(navegador, 10)
 
 
 
-
 #NOTA: O código abaixo é um exemplo de como NÃO se deve fazer, pois ele tenta localizar os elementos antes mesmo de eles estarem presentes na página, o que pode levar a erros de "element not found".
 #ERRADO > filmes_find_element = wait.until(EC.presence_of_all_elements_located(navegador.find_elements((By.XPATH, "//li[.//h3[contains(@class, 'ipc-title__text')]]"))))
 #ERRADO > filmes_find_element_no_wait = navegador.find_elements(By.XPATH, "//li[.//h3[contains(@class, 'ipc-title__text')]]")
@@ -48,22 +48,6 @@ def close_promptable():
     except TimeoutException:
         print("Erro: não foi possível abrir o pop-up de informações.")
 
-    #DEPRECATED > Estrategia 1: botão com data-testid específico
-    #Não vai funcionar pois a tag @data-testid não pertence ao botão de fechar, mas sim à div pai do pop-up(duhh).
-    '''
-    try:
-        # Estrategia 1: botão com data-testid específico
-        close_button = wait.until(
-            EC.element_to_be_clickable(
-                (By.XPATH, ".//button[@data-testid='title-summary-prompt-close-button']")
-            )
-        )
-        close_button.click()
-        print("Botão fechado com data-testid.")
-        return
-    except TimeoutException:
-        pass  # Continua para a próxima tentativa
-    '''
     try:
         #Botão com tag aria-label="Fechar" ou "Close"
         close_btn = wait.until(
@@ -73,45 +57,50 @@ def close_promptable():
         print("Popup fechado com aria-label.")
         return
     except TimeoutException:
-        pass  # Continua para a próxima tentativa
+        pass  
     
     try:
-        # Estratégia 4: clicar no fundo escuro (overlay) para fechar
-        overlay = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@role='presentation' and contains(@class, 'ipc-promptable-base__backdrop')]")))
-        overlay.click()
-        print("Popup fechado clicando no overlay.")
+        #Envia a tecla escape para o body
+        navegador.find_element(By.TAG_NAME, 'body').send_keys(Keys.ESCAPE)
+        print("Popup fechado clicando escape.")
     except TimeoutException:
         print("Erro: não foi possível fechar o pop-up.")
 
 
 
 
-##### Iniciando o processo de scrapping #####
-
-movie_list = wait.until(
-    EC.presence_of_all_elements_located(
-        (By.XPATH, '//div[@data-testid="chart-layout-main-column"]//li[contains(@class,"ipc-metadata-list-summary-item")]')
+##### Rotina de processo de scrapping #####
+def main():
+    movie_list = wait.until(
+        EC.presence_of_all_elements_located(
+            (By.XPATH, '//div[@data-testid="chart-layout-main-column"]//li[contains(@class,"ipc-metadata-list-summary-item")]')
+        )
     )
-)
-print(f"Total de filmes encontrados: {len(movie_list)}")
+    print(f"Total de filmes encontrados: {len(movie_list)}")
 
-for idx, movie in enumerate(movie_list):
+    for idx, movie in enumerate(movie_list, start=1):
 
-    #Centraliza o filme na tela para garantir que o botão de informações esteja visível
-    navegador.execute_script("arguments[0].scrollIntoView({block: 'center'});", movie)
+        #Centraliza o filme na tela para garantir que o botão de informações esteja visível
+        navegador.execute_script("arguments[0].scrollIntoView({block: 'center'});", movie)
 
-    #Localiza o botão de informações usando um XPath relativo ao elemento do filme
-    info_button = movie.find_element(By.XPATH, './/button[@data-testid="title-summary-prompt-button-info-icon"]')
-    print(f"\nFilme atual: {movie.find_element(By.XPATH, './/h3[contains(@class, "ipc-title__text")]').text}")
-    info_button.click()
-    #
-    #tratamento das funcoes
-    #
+        #Localiza o botão de informações usando um XPath relativo ao elemento do filme
+        info_btn = wait.until(
+            EC.element_to_be_clickable((By.XPATH, ".//button[@data-testid='title-summary-prompt-button-info-icon']"))
+        )
+        print(f"\nFilme {idx}: {movie.find_element(By.XPATH, './/h3[contains(@class, "ipc-title__text")]').text}")
+        info_btn.click()
+        
+        ####
+        #Tratamento das funcoes de json 
+        ####
 
-    close_promptable()
+        close_promptable()
 
-print(f"Filmes na movie_list: {len(movie_list)}")
+    print(f"Filmes na movie_list: {len(movie_list)}")
 
+#### Iniciando o processo de scrapping chamando a função main() - python main.py no terminal####
+if __name__ == "__main__":
+    main()
 
 '''
 #Utiliza o beautifulsoup para pegar as informações
